d235d4f8a6422878027292dd226ed4ed28171fc5
mu0z <mo86@gmx.com>
2025-10-21 09:30:41 +0000

    Creation of a program that prints a string using the printf function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * the function prints a string using printf followed by a newline & return 0
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")\n;
+
+	return (0);
+}
