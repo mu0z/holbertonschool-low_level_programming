d9bbcc5ce1c7be4135f3c2e337f04ea6c9671160
mu0z <mo86@gmx.com>
2025-10-21 09:20:53 +0000

    Creation of a program that prints a string with the puts function

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * The function prints a string using puts & returns 0
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
