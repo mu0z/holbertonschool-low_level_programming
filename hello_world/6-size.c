3fc4463f017828fa949b13fd747d4a0017eea7f5
mu0z <mo86@gmx.com>
2025-10-21 09:42:21 +0000

    Creation of a script that prints the size of various types

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * the function prints the size of various types
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	
+	return (0);
+}
