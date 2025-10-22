39369f9cbdad8cf59bf98c1de7dfbf653e794112
mu0z <mo86@gmx.com>
2025-10-22 12:31:37 +0000

    Print if last number of random n variable is greater than 5 or less than 6

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,14 +18,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	printf("Last digit of %d is %d" , n, last_digit);
-
 	if (last_digit > 5)
-		printf("and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, last_digit);
 	else if (last_digit == 0)
-		printf("and is 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else
-		printf("and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
+			, n, last_digit);
 
 	return (0);
 }
