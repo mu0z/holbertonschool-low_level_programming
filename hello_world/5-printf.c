35ff0a070671c5852b5d7b71606a13cd5454ef8a
mu0z <mo86@gmx.com>
2026-02-25 15:18:24 +0100

    add c prog that prints text with a newline using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * print a string using printf & return 0
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
