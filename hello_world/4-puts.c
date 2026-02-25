9773a90eb2916b741a9f550f592a0232539c92e0
mu0z <mo86@gmx.com>
2026-02-25 15:00:28 +0100

    t4 - add c prog that prints a sentence using puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Print a string using the function puts & return 0
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
