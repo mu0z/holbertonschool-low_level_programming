ff949ee7abbffba3ae22ff6d914201718f71bdd4
mu0z <mo86@gmx.com>
2026-02-25 23:34:57 +0100

    t3 - add program that prints alphabet in lower and uppercase followed by a newline

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints the alphabet in lowercase and uppercase followed by a \n
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char character;
+	char character2;
+
+	character = 'a';
+	character2 = 'A';
+
+	while (character <= 'z')
+	{
+		putchar(character);
+		character++;
+	}
+
+	while (character2 <= 'Z')
+        {
+                putchar(character2);
+                character2++;
+        }
+
+	putchar('\n');
+
+	return (0);
+}
