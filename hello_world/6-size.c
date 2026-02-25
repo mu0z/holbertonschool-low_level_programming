fe2b5ba595af8bab0c635d73a34eedd89292205a
mu0z <mo86@gmx.com>
2026-02-25 15:37:04 +0100

    t6 - add c prog that prints the size of different types

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * print the size of various types using printf and the sizeof operator
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
