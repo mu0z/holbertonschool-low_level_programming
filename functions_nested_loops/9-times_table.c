6e6f186f44587284df3ab17aeeae052675eb3e67
mu0z <mo86@gmx.com>
2026-02-27 14:55:58 +0100

    add prototype that prints the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table -
+ */
+
+void times_table(void)
+{
+	int row;
+	int col;
+	int prod;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			prod = row * col;
+
+			if (col == 0)
+			{
+				_putchar(prod + '0');
+			}
+			if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			if (col == 9)
+				_putchar('\n');
+		}
+	}
+}
+
