91cc1586591d0e5c1bc7447ffa0bc3365a14067d
mu0z <mo86@gmx.com>
2026-03-02 00:13:28 +0100

    added separate if statement for space character

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,25 @@ int main(void)
 
 	for (n = 1 ; n <= 100 ; n++)
 	{
-		if (n % 3 == 0 && n % 5 != 0)
-		{
-			printf("Fizz ");
-		}
-		else if (n % 5 == 0 && n % 3 != 0)
-		{
-			printf("Buzz ");
-		}
-		else if (n % 3 == 0 && n % 5 == 0 && n != 100)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (n == 100)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
+		}
+		if (n < 100)
+		{
+			printf(" ");
 		}
 	}
 	return (0);
