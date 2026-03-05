b2ef39041ae39c071e04673b353071bcd011621a
mu0z <mo86@gmx.com>
2026-03-05 10:14:02 +0100

    add basic calculator with menu

---
+++ simple_calculator/calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int choice;
+
+	printf("~~Simple Calculator~~\n");
+	printf("[1] - Add\n");
+	printf("[2] - Substract\n");
+	printf("[3] - Multiply\n");
+	printf("[4] - Divide\n");
+	printf("[0] - Quit\n");
+	
+	printf("Choice : ");
+	scanf("%d", &choice);
+
+	if (choice == 0)
+		printf("Goodbye !\n");
+	return (0);
+}
