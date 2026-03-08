51f39a11a3f62d5e9b27e3971bb80cefa23be67d
mu0z <mo86@gmx.com>
2026-03-08 21:14:54 +0100

    add function that prints every two character of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every 2 characters of a string
+ * @str: pointer to the string
+ */
+
+void puts2(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+		_putchar(str[s]);
+		s++;
+		s++;
+	}
+	_putchar('\n');
+}
